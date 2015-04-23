8698e65b014a2fd2b47a462e972c069ec6ada751
Farid Rn <faridv@gmail.com>
2015-04-24 00:29:02 +0430

    shared blocks added

---
+++ blocks/shared/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>HispanTV</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--<link rel="stylesheet/less" href="less/hispan.less" />-->
+        <link rel="stylesheet" href="css/hispan.css" />
+        <link rel="shortcut icon" href="favicon.ico"/>
+        <!--[if lt IE 10]>
+        <script src="global-v2/js/modernizr-2.6.2.min.js"></script><![endif]-->
+<!--        <script>
+            less = {env: "development", logLevel: 1, async: false, fileAsync: false, dumpLineNumbers: "mediaquery", relativeUrls: false, rootpath: ""};
+        </script>
+        <script src="js/dev/less-1.7.4.min.js"></script>-->
---
+++ blocks/shared/header.php
@@ -0,0 +1,63 @@
+        <header id="header">
+            <div id="navbar" class="wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-3 col-md-2">
+                            <h1 id="logo">
+                                <a href="#">HispanTV</a>
+                            </h1>
+                        </div>
+                        <div class="col-xs-8 col-sm-9 col-md-10">
+                            <nav id="menu" class="pull-right">
+                                <ul class="list-unstyled list-inline">
+                                    <li class="hascild"><a href="#">Noticias</a></li>
+                                    <li class="hascild active"><a href="#">Programas</a></li>
+                                    <li class=""><a href="#">Documentales</a></li>
+                                    <li><a href="#">Reportajes</a></li>
+                                    <li class=""><a href="#">peliculas</a></li>
+                                    <li class=""><a href="#">Series</a></li>
+                                    <li class="search"><a href="#"><i class="icon-search"></i></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="masthead" class="wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-8 col-md-offset-4">
+                            <div class="pull-right">
+                                <ul class="list-unstyled list-inline">
+                                    <li><a href="#" class="btn btn-danger btn-xs">TV en Directo <i class="icon-tv"></i></a></li>
+                                    <li><a href="#">Seas Reporteras</a></li>
+                                    <li><a href="#">Distribución</a></li>
+                                    <li><a href="#">Programación</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="ticker" class="wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="inner row">
+                                <div class="col-xs-4 col-sm-3 col-md-2"><div class="header"><span>Ultimas Hura</span></div></div>
+                                <div class="titles col-xs-8 col-sm-9 col-md-10">
+                                    <div class="titles">
+                                        <ul class="list-unstyled">
+                                            <li><a href="#">Some title goes here</a></li>
+                                            <li><a href="#">This place will show breaking news</a></li>
+                                            <li><a href="#">‘Paz sostenible en Palestina requiere de vía democrática’</a></li>
+                                            <li><a href="#">Israel ofrece dinero a palestinos para que abandonen su tierra</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header><!-- ./header -->

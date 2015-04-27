19aba6d684dc35350d3ac0dd3b24bdcd7961a93f
Farid Rn <faridv@gmail.com>
2015-04-27 22:37:18 +0430

    Showcase module

---
+++ blocks/modules/showcase.php
@@ -0,0 +1,23 @@
+<div id="showcase" class="row">
+	<div class="col-xs-12">
+		<div class="panel top-news">
+			<div class="panel-body">
+				<div class="item">
+					<div class="inner">
+						<div class="img video">
+							<img src="data/smpl10.jpg" alt="" />
+						</div>
+						<div class="section green">
+							<a href="#">United States</a>
+						</div>
+						<div class="title">
+							<h2>
+								<a href="#">Operación ‘Mare Nostrum’ está  en una situación de entredicho</a>
+							</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

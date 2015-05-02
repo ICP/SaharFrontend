5464548e8e514105c11af134fb4d8471da1165b7
Farid Rn <faridv@gmail.com>
2015-05-02 21:04:31 +0430

    + Item page

---
+++ blocks/item.php
@@ -0,0 +1,346 @@
+<div id="item" class="item">
+	<div class="page-header">
+		<h3>
+			<a href="#">United States</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a href="#">Politics</a>
+		</h3>
+		<h2 class="item-title">Operación ‘Mare Nostrum’ está en una am situación de entredicho</h2>
+	</div>
+	<div class="item-media">
+		<?php include dirname(__FILE__) . '/modules/item-media.php' ?><!-- ./apps -->
+	</div>
+	<div class="item-body">
+		<div class="row">
+			<div class="col-xs-12 col-sm-8">
+				<div class="item-text">
+					<p class="introtext">Frank Emspak, activista estadounidense pro derechos humanos, ha señalado que la desigualdad en los salarios y la distribución de la riqueza en EE.UU. ha sido una de las principales causas de la recesión económica en el país.</p>
+					<p>Frank Emspak, activista estadounidense pro derechos humanos, ha señalado que la desigualdad en los salarios y la distribución de la riqueza en EE.UU. ha sido una de las principales causas de la recesión económica en el país. </p>
+					<p>En una entrevista concedida a la televisión iraní en inglés, PressTV, Emspak ha asegurado que ninguna sociedad con este nivel de desigualdad social y económica que azota a EE.UU., ha podido sobrevivir durante mucho tiempo. </p>
+					<p>Una situación tal se vivió también, según el activista, durante la década de 1890, cuando el descontento por los salarios bajos y las condiciones laborales insalubres desataron una ola de paros en el trabajo industrial e hicieron que la población votara a favor de los socialistas. </p>
+					<p>Algo parecido tuvo lugar en EE.UU. en 1929, cuando a pesar del crecimiento económico, el incremento de la riqueza entre las capas sociales más ricas y el aumento de la oferta de productos en el mercado, la mayoría de la población era incapaz de ganarse la vida, y eso se debía a los sueldos fijos, mientras que los precios habían subido, explicó Emspak. </p>
+					<p>En una entrevista concedida a la televisión iraní en inglés, PressTV, Emspak ha asegurado que ninguna sociedad con este nivel de desigualdad social y económica que azota a EE.UU., ha podido sobrevivir durante mucho tiempo. </p>
+					<blockquote>económico, el incremento de la riqueza entre las capas sociales más ricas y el aumento de la oferta de productos en el mercado, la mayoría de la población era entre las capas sociales más ricas y el aumento de la oferta de productos en el mercado, la  ayoría de la población era ubido, explicó Emspak.a población era entre las capas sociales más ricas.</blockquote>
+					<p>Una situación tal se vivió también, según el activista, durante la década de 1890, cuando el descontento por los salarios bajos y las condiciones laborales insalubres desataron una ola de paros en el trabajo industrial e hicieron que la población votara a favor de los socialistas.</p>
+					<h4>Title of this part</h4>
+					<p>En una entrevista concedida a la televisión iraní en inglés, PressTV, Emspak ha asegurado que ninguna sociedad con este nivel de desigualdad social y económica que azota a EE.UU., ha podido sobrevivir durante mucho tiempo. </p>
+					<p>Una situación tal se vivió también, según el activista, durante la década de 1890, cuando el descontento por los salarios bajos y las condiciones laborales insalubres desataron una ola de paros en el trabajo industrial e hicieron que la población votara a favor de los socialistas. </p>
+					<p>Algo parecido tuvo lugar en EE.UU. en 1929, cuando a pesar del crecimiento económico, el incremento de la riqueza entre las capas sociales más ricas y el aumento de la oferta de productos en el mercado, la mayoría de la población era incapaz de ganarse la vida, y eso se debía a los sueldos fijos, mientras que los precios habían subido, explicó Emspak. </p>
+					<p>En una entrevista concedida a la televisión iraní en inglés, PressTV, Emspak ha asegurado que ninguna sociedad con este nivel de desigualdad social y económica que azota a EE.UU., ha podido sobrevivir durante mucho tiempo. </p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4 item-sidebar">
+				<div class="item-author">By <a href="#">Harry Potter</a></div>
+				<div class="item-date">Thu Jan 23, 2014 9:01 AM GMT</div>
+				<div class="item-tools">
+					<div class="row font-resizer">
+						<div class="hidden-xs hidden-sm col-md-12">
+							<div class="items-filter">
+								<ul class="list-unstyled list-inline">
+									<li><a href="#" class="smaller">a</a></li>
+									<li class="active"><a href="#" class="reset">•</a></li>
+									<li><a href="#" class="larger">A</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="row download-media">
+						<div class="hidden-xs hidden-sm col-md-12">
+							<a href="#" class="btn btn-default">Download</a>
+						</div>
+					</div>
+					<div class="row download-media">
+						<div class="col-xs-12">
+							<a href="#" class="btn btn-default">View in <i class="icon-youtube"></i></a>
+						</div>
+					</div>
+					<div class="row print">
+						<div class="col-xs-12">
+							<ul class="list-inline list-unstyled">
+								<li><a href="#"><i class="icon-print"></i> Print</a></li>
+								<li><a href="#"><i class="icon-envelope"></i> Email</a></li>
+								<li><a href="#"><i class="icon-code"></i> Embed</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="row shares">
+						<div class="col-xs-12">
+							<div class="panel share">
+								<div class="panel-body">
+									<ul class="list-unstyled list-inline">
+										<li><a href="#"><i class="icon-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-twitter"></i></a></li>
+										<li><a href="#"><i class="icon-gplus"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<h3>Tags</h3>
+				<div class="items-tags">
+					<ul class="list-unstyled list-inline">
+						<li><a href="#">Irán</a></li>
+						<li class="active"><a href="#">Las Américas</a></li>
+						<li><a href="#">EE.UU. y Canadá</a></li>
+						<li><a href="#">Oriente Medio</a></li>
+						<li><a href="#">Europa</a></li>
+						<li><a href="#">Sociedad</a></li>
+						<li><a href="#">Economía</a></li>
+						<li><a href="#">Cultura</a></li>
+					</ul>
+				</div>
+				<div class="item-about-author">
+					<p><strong>Mr. Harry Potter</strong> is a Algo parecido tuvo lugar en EE.UU. en 1929, cuando a pesar del crecimiento económico, el incremento de la riqueza entre las capas sociales más ricas y el aumento de la oferta de productos en el mercado, la mayoría de la población era incapaz de ganarse la vida, y eso se debía a los sueldos fijos, mientras que los precios habían subido, explicó Emspak. </p>
+				</div>
+				<div class="item-sharings">
+					<div class="panel list sharings">
+						<div class="panel-body">
+							<ul class="list-unstyled list-inline">
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
+								<li><a href="#"><i class="icon-gplus"></i></a></li>
+								<li><a href="#"><i class="icon-digg"></i></a></li>
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
+								<li><a href="#"><i class="icon-delicious"></i></a></li>
+								<li><a href="#"><i class="icon-pinterest"></i></a></li>
+								<li><a href="#"><i class="icon-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="item-related">
+					<div class="panel title-border tiles">
+						<div class="panel-title">
+							<span>More from Iran</span>
+						</div>
+						<div class="panel-body">
+							<div class="row">
+								<ul class="list-unstyled">
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+									<li class="tile col-xs-12 col-sm-4">
+										<div class="inner">
+											<div class="img video">
+												<a href="#">
+													<img src="data/smpl8.jpg" alt="" />
+												</a>
+											</div>
+											<div class="desc">
+												<h4><a href="#">Cine a Contra Corriente</a></h4>
+											</div>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-12">
+						<div class="item-comments">
+							<div class="panel title-border comments">
+								<div class="panel-title">
+									<span>Commentarios</span>
+								</div>
+								<div class="panel-body">
+									<div role="tabpanel">
+										<!-- Nav tabs -->
+										<ul class="nav nav-tabs" role="tablist">
+											<li role="presentation"><a href="#facebook-comments" role="tab" data-toggle="tab">Fcebook Comments ()</a></li>
+											<li role="presentation" class="active"><a href="#site-comments" role="tab" data-toggle="tab">Our Comments (72)</a></li>
+										</ul>
+										<!-- Tab panes -->
+										<div class="tab-content">
+											<div role="tabpanel" class="tab-pane fade in" id="facebook-comments">...</div>
+											<div role="tabpanel" class="tab-pane active fade in" id="site-comments">
+												<div class="panel comment-form">
+													<div class="panel-body">
+														<h4>72 Comments</h4>
+														<div class="login">Login Tools</div>
+														<form role="form" class="comment-form-body">
+															<div class="avatar pull-left"></div>
+															<div class="input">
+																<div class="form-group">
+																	<textarea class="form-control" name="comment" placeholder="Your comment text goes here..."></textarea>
+																	<div class="button-holder">
+																		<a href="#"><i class="icon-picture"></i></a>
+																	</div>
+																</div>
+															</div>
+														</form>
+													</div>
+												</div>
+												<div class="panel comments-list">
+													<ul class="list-unstyled comment-items">
+														<li class="comment-item">
+															<div class="comment-header">
+																<span class="avatar"></span>
+																<span class="name">Farid</span>
+																<div class="comment-rate pull-right">
+																	<span class="rate-count">120</span>
+																	<a  class="vote-up"href="#"><i class="icon-thumbs-up"></i></a>
+																	<a class="vote-down" href="#"><i class="icon-thumbs-down"></i></a>
+																</div>
+															</div>
+															<div class="comment-text">
+																<p>It is interesting that Israel seems to have very little problem with Wahhabism or, for that matter, the Saudi leadership. Is there a convergence here?</p>
+																<div class="date pull-left">Thu Jan 23, 2014 9:01 AM GMT</div>
+																<div class="comment-reply pull-right">
+																	<a href="#">Reply</a>
+																</div>
+															</div>
+															<div class="clearfix"></div>
+															<ul class="list-unstyled comment-items quotes">
+																<li class="comment-item quote">
+																	<div class="comment-header">
+																		<span class="avatar"></span>
+																		<span class="name">Farid</span>
+																		<div class="comment-rate pull-right">
+																			<span class="rate-count">120</span>
+																			<a  class="vote-up"href="#"><i class="icon-thumbs-up"></i></a>
+																			<a class="vote-down" href="#"><i class="icon-thumbs-down"></i></a>
+																		</div>
+																	</div>
+																	<div class="comment-text">
+																		<p>It is interesting that Israel seems to have very little problem with Wahhabism or, for that matter, the Saudi leadership. Is there a convergence here?</p>
+																		<div class="date pull-left">Thu Jan 23, 2014 9:01 AM GMT</div>
+																		<div class="comment-reply pull-right">
+																			<a href="#">Reply</a>
+																		</div>
+																	</div>
+																	<div class="clearfix"></div>
+																</li>
+															</ul>
+														</li>
+														<li class="comment-item">
+															<div class="comment-header">
+																<span class="avatar"></span>
+																<span class="name">Farid</span>
+																<div class="comment-rate pull-right">
+																	<span class="rate-count">120</span>
+																	<a  class="vote-up"href="#"><i class="icon-thumbs-up"></i></a>
+																	<a class="vote-down" href="#"><i class="icon-thumbs-down"></i></a>
+																</div>
+															</div>
+															<div class="comment-text">
+																<p>It is interesting that Israel seems to have very little problem with Wahhabism or, for that matter, the Saudi leadership. Is there a convergence here?</p>
+																<div class="date pull-left">Thu Jan 23, 2014 9:01 AM GMT</div>
+																<div class="comment-reply pull-right">
+																	<a href="#">Reply</a>
+																</div>
+															</div>
+															<div class="clearfix"></div>
+															<ul class="list-unstyled comment-items quotes">
+																<li class="comment-item quote">
+																	<div class="comment-header">
+																		<span class="avatar"></span>
+																		<span class="name">Farid</span>
+																		<div class="comment-rate pull-right">
+																			<span class="rate-count">120</span>
+																			<a  class="vote-up"href="#"><i class="icon-thumbs-up"></i></a>
+																			<a class="vote-down" href="#"><i class="icon-thumbs-down"></i></a>
+																		</div>
+																	</div>
+																	<div class="comment-text">
+																		<p>It is interesting that Israel seems to have very little problem with Wahhabism or, for that matter, the Saudi leadership. Is there a convergence here?</p>
+																		<div class="date pull-left">Thu Jan 23, 2014 9:01 AM GMT</div>
+																		<div class="comment-reply pull-right">
+																			<a href="#">Reply</a>
+																		</div>
+																	</div>
+																	<div class="clearfix"></div>
+																</li>
+																<li class="comment-item quote">
+																	<div class="comment-header">
+																		<span class="avatar"></span>
+																		<span class="name">Farid</span>
+																		<div class="comment-rate pull-right">
+																			<span class="rate-count">120</span>
+																			<a  class="vote-up"href="#"><i class="icon-thumbs-up"></i></a>
+																			<a class="vote-down" href="#"><i class="icon-thumbs-down"></i></a>
+																		</div>
+																	</div>
+																	<div class="comment-text">
+																		<p>It is interesting that Israel seems to have very little problem with Wahhabism or, for that matter, the Saudi leadership. Is there a convergence here?</p>
+																		<div class="date pull-left">Thu Jan 23, 2014 9:01 AM GMT</div>
+																		<div class="comment-reply pull-right">
+																			<a href="#">Reply</a>
+																		</div>
+																	</div>
+																	<div class="clearfix"></div>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ blocks/modules/item-media.php
@@ -0,0 +1,24 @@
+<div id="slideshow" class="row">
+	<div class="col-xs-12">
+		<div class="panel slideshow">
+			<div class="panel-body">
+				<div class="carousel-control">
+					<a href="#" class="prev"><i class="icon-left"></i></a>
+					<a href="#" class="next"><i class="icon-right"></i></a>
+				</div>
+				<ul class="list-unstyled">
+					<li class="item">
+						<div class="inner">
+							<div class="img">
+								<img src="data/smpl11.jpg" alt="" />
+							</div>
+							<div class="title">
+								<h2>Description</h2>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>

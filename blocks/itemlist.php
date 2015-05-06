890788fd812ec642f4c6c75a47c4cb1db776c793
Farid Rn <faridv@gmail.com>
2015-05-07 00:35:29 +0430

    + Itemlist page

---
+++ blocks/itemlist.php
@@ -0,0 +1,125 @@
+<div class="page-header">
+	<h2>News</h2>
+</div>
+<div class="items-filter">
+	<ul class="list-unstyled list-inline">
+		<li class="active"><a href="#">All</a></li>
+		<li><a href="#">Subcategory</a></li>
+		<li><a href="#">Subcategory</a></li>
+		<li><a href="#">Subcategory</a></li>
+		<li><a href="#">Subcategory</a></li>
+		<li><a href="#">Subcategory</a></li>
+		<li><a href="#">Subcategory</a></li>
+	</ul>
+</div>
+<div id="itemlist">
+	<div class="panel itemlist thumb thumbs-large">
+		<div class="panel-body">
+			<ul class="list-unstyled">
+				<li class="featured">
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img video">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="inner">
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="panel-footer">
+			<div class="more text-center has-line">
+				<a class="btn black" href="#">More</a>
+			</div>
+		</div>
+	</div>
+</div>

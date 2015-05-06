5f0d221a43e9fac3249d6ceb644cbb5f91fd5553
Farid Rn <faridv@gmail.com>
2015-05-07 00:38:57 +0430

    Changes in latest news module tabs

--- blocks/modules/sidebar/latest.php
+++ blocks/modules/sidebar/latest.php
@@ -1,79 +1,78 @@
 <div id="latest" class="row">
-    <div class="col-xs-12">
-        <div class="panel title-tabs itemlist thumb tabs">
-            <div class="panel-title">
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="tab-2 active"><a href="#ultimas" role="tab" data-toggle="tab"><span>Latest News</span></a></li>
-                    <li class="tab-1"><a href="#breves" role="tab" data-toggle="tab" data-catid="101" data-params='{{Config.Site_Modules_Params}}' data-template="ContentsList\Itemlist"><span>Briefs</span></a></li>
-                    <li class="tab-3"><a href="#lomas" role="tab" data-toggle="tab" data-type="popular" data-load=".sub-tabs"><span>Most Read</span></a></li>
-                </ul>
-            </div>
-            <div class="panel-body">
-                <div class="tab-content">
-                    <div class="tab-pane active fade in" id="ultimas">
-                        <ul class="list-unstyled">
-                            <li>
-                                <div class="inner">
-                                    <a href="#">
-                                        <span class="img"><img src="data/smpl2.jpg" alt="" /></span>
-                                        <span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
-                                    </a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="inner">
-                                    <a href="#">
-                                        <span class="img"><img src="data/smpl1.jpg" alt="" /></span>
-                                        <span class="title">Operación ‘Mare Nostrum’ está en una situación de entredicho</span>
-                                    </a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="inner">
-                                    <a href="#">
-                                        <span class="img"><img src="data/smpl3.jpg" alt="" /></span>
-                                        <span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
-                                    </a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="inner">
-                                    <a href="#">
-                                        <span class="img"><img src="data/smpl9.jpg" alt="" /></span>
-                                        <span class="title">Operación ‘Mare Nostrum’ está en una situación de entredicho</span>
-                                    </a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="inner">
-                                    <a href="#">
-                                        <span class="img"><img src="data/smpl5.jpg" alt="" /></span>
-                                        <span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                        <a href="/noticias" class="more">More</a>
-                    </div>
-                    <div class="tab-pane fade" id="breves">
-                        <ul class="list-unstyled"></ul>
-                    </div>
-                    <div class="tab-pane fade" id="lomas">
-                        <div class="text-center">
-                            <ul class="sub-tabs list-inline list-unstyled">
-                                <li class="active"><a href="#popular1" data-time="24">Today</a></li>
-                                <li><a href="#popular2" data-time="168">Last Week</a></li>
-                                <li><a href="#popular3" data-time="720">last Month</a></li>
-                            </ul>
-                            <div class="sub-tab-content text-left">
-                                <div id="popular1"><ul class="list-unstyled"></ul></div>
-                                <div id="popular2"><ul class="list-unstyled"></ul></div>
-                                <div id="popular3"><ul class="list-unstyled"></ul></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="col-xs-12">
+		<div class="panel itemlist thumb tabs">
+			<div class="panel-title">
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="tab-2 active"><a href="#ultimas" role="tab" data-toggle="tab"><span>Latest News</span></a></li>
+					<li class="tab-3"><a href="#lomas" role="tab" data-toggle="tab" data-type="popular" data-load=".sub-tabs"><span>Most Read</span></a></li>
+				</ul>
+			</div>
+			<div class="panel-body">
+				<div class="tab-content">
+					<div class="tab-pane active fade in" id="ultimas">
+						<ul class="list-unstyled">
+							<li>
+								<div class="inner">
+									<a href="#">
+										<span class="img"><img src="data/smpl2.jpg" alt="" /></span>
+										<span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
+									</a>
+								</div>
+							</li>
+							<li>
+								<div class="inner">
+									<a href="#">
+										<span class="img"><img src="data/smpl1.jpg" alt="" /></span>
+										<span class="title">Operación ‘Mare Nostrum’ está en una situación de entredicho</span>
+									</a>
+								</div>
+							</li>
+							<li>
+								<div class="inner">
+									<a href="#">
+										<span class="img"><img src="data/smpl3.jpg" alt="" /></span>
+										<span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
+									</a>
+								</div>
+							</li>
+							<li>
+								<div class="inner">
+									<a href="#">
+										<span class="img"><img src="data/smpl9.jpg" alt="" /></span>
+										<span class="title">Operación ‘Mare Nostrum’ está en una situación de entredicho</span>
+									</a>
+								</div>
+							</li>
+							<li>
+								<div class="inner">
+									<a href="#">
+										<span class="img"><img src="data/smpl5.jpg" alt="" /></span>
+										<span class="title">Aumenta 228 cifra de muetros  por ia du naufragio del ferry sucoreano</span>
+									</a>
+								</div>
+							</li>
+						</ul>
+						<a href="/noticias" class="more">More</a>
+					</div>
+					<div class="tab-pane fade" id="breves">
+						<ul class="list-unstyled"></ul>
+					</div>
+					<div class="tab-pane fade" id="lomas">
+						<div class="text-center">
+							<ul class="sub-tabs list-inline list-unstyled">
+								<li class="active"><a href="#popular1" data-time="24">Today</a></li>
+								<li><a href="#popular2" data-time="168">Last Week</a></li>
+								<li><a href="#popular3" data-time="720">last Month</a></li>
+							</ul>
+							<div class="sub-tab-content text-left">
+								<div id="popular1"><ul class="list-unstyled"></ul></div>
+								<div id="popular2"><ul class="list-unstyled"></ul></div>
+								<div id="popular3"><ul class="list-unstyled"></ul></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>

2e22e7efe3c6dd0a5c227566d96207a65f89c825
Farid Rn <faridv@gmail.com>
2015-05-09 22:35:42 +0430

    minor responsive bugfiz in programs module

--- blocks/modules/programs.php
+++ blocks/modules/programs.php
@@ -1,114 +1,114 @@
 <div id="programs" class="row hidden-sm hidden-xs">
-                                <div class="col-xs-6">
-                                    <div class="panel tabs">
-                                        <div class="panel-title">
-                                            <ul class="nav nav-tabs" role="tablist">
-                                                <li class="active"><a href="#programas" role="tab" data-toggle="tab"><span>Programas</span></a></li>
-                                                <li><a href="#documentales" role="tab" data-toggle="tab"><span>Documentales</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="panel-body">
-                                            <div class="tab-content">
-                                                <div class="tab-pane active fade in" id="programas">
-                                                    <div class="inner">
-                                                        <div class="row">
-                                                            <div class="col-xs-6">
-                                                                <div class="item video medium">
-                                                                    <a href="#">
-                                                                        <span class="img">
-                                                                            <img src="data/smpl9.jpg" alt="" />
-                                                                        </span>
-                                                                        <span class="title">Irán Hoy</span>
-                                                                        <span class="subtitle">Episode 12</span>
-                                                                    </a>
-                                                                    <div class="air-table">
-                                                                        <ul class="list-unstyled">
-                                                                            <li><a href="#">Monday 12:00 GMT</a></li>
-                                                                            <li><a href="#">Monday 12:00 GMT</a></li>
-                                                                            <li>Monday 12:00 GMT</li>
-                                                                            <li>Monday 12:00 GMT</li>
-                                                                        </ul>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-xs-6">
-                                                                <div class="itemlist thumb">
-                                                                    <div class="panel-body">
-                                                                        <ul class="list-unstyled">
-                                                                            <li>
-                                                                                <div class="inner">
-                                                                                    <a href="#">
-                                                                                        <span class="img"><img src="data/smpl8.jpg" alt="" /></span>
-                                                                                        <span class="title">Name or title of the program</span>
-                                                                                    </a>
-                                                                                </div>
-                                                                            </li>
-                                                                            <li>
-                                                                                <div class="inner">
-                                                                                    <a href="#">
-                                                                                        <span class="img"><img src="data/smpl7.jpg" alt="" /></span>
-                                                                                        <span class="title">Name or title of the program</span>
-                                                                                    </a>
-                                                                                </div>
-                                                                            </li>
-                                                                            <li>
-                                                                                <div class="inner">
-                                                                                    <a href="#">
-                                                                                        <span class="img"><img src="data/smpl6.jpg" alt="" /></span>
-                                                                                        <span class="title">Name or title of the program</span>
-                                                                                    </a>
-                                                                                </div>
-                                                                            </li>
-                                                                            <li>
-                                                                                <div class="inner">
-                                                                                    <a href="#">
-                                                                                        <span class="img"><img src="data/smpl5.jpg" alt="" /></span>
-                                                                                        <span class="title">Name or title of the program</span>
-                                                                                    </a>
-                                                                                </div>
-                                                                            </li>
-                                                                        </ul>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="tab-pane fade" id="documentales">
-                                                    documentales
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xs-6">
-                                    <div class="panel tabs">
-                                        <div class="panel-title">
-                                            <ul class="nav nav-tabs" role="tablist">
-                                                <li class="active"><a href="#peliculas" role="tab" data-toggle="tab"><span>Peliculas</span></a></li>
-                                                <li><a href="#series" role="tab" data-toggle="tab"><span>Series</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="panel-body">
-                                            <div class="tab-content">
-                                                <div class="tab-pane active fade in" id="peliculas">
-                                                    <div class="item video big">
-                                                        <a href="#">
-                                                            <span class="img">
-                                                                <img src="data/smpl3.jpg" alt="" />
-                                                            </span>
-                                                            <span class="title-holder">
-                                                                <span class="title">Irán Hoy</span>
-                                                                <span class="subtitle">Calurosa bienvenida a la 27º Feria del Libro de Teherán</span>
-                                                            </span>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                <div class="tab-pane fade" id="series">
-                                                    series
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+	<div class="col-xs-12 col-sm-6">
+		<div class="panel tabs">
+			<div class="panel-title">
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="active"><a href="#programas" role="tab" data-toggle="tab"><span>Programas</span></a></li>
+					<li><a href="#documentales" role="tab" data-toggle="tab"><span>Documentales</span></a></li>
+				</ul>
+			</div>
+			<div class="panel-body">
+				<div class="tab-content">
+					<div class="tab-pane active fade in" id="programas">
+						<div class="inner">
+							<div class="row">
+								<div class="col-xs-6">
+									<div class="item video medium">
+										<a href="#">
+											<span class="img">
+												<img src="data/smpl9.jpg" alt="" />
+											</span>
+											<span class="title">Irán Hoy</span>
+											<span class="subtitle">Episode 12</span>
+										</a>
+										<div class="air-table">
+											<ul class="list-unstyled">
+												<li><a href="#">Monday 12:00 GMT</a></li>
+												<li><a href="#">Monday 12:00 GMT</a></li>
+												<li>Monday 12:00 GMT</li>
+												<li>Monday 12:00 GMT</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-6">
+									<div class="itemlist thumb">
+										<div class="panel-body">
+											<ul class="list-unstyled">
+												<li>
+													<div class="inner">
+														<a href="#">
+															<span class="img"><img src="data/smpl8.jpg" alt="" /></span>
+															<span class="title">Name or title of the program</span>
+														</a>
+													</div>
+												</li>
+												<li>
+													<div class="inner">
+														<a href="#">
+															<span class="img"><img src="data/smpl7.jpg" alt="" /></span>
+															<span class="title">Name or title of the program</span>
+														</a>
+													</div>
+												</li>
+												<li>
+													<div class="inner">
+														<a href="#">
+															<span class="img"><img src="data/smpl6.jpg" alt="" /></span>
+															<span class="title">Name or title of the program</span>
+														</a>
+													</div>
+												</li>
+												<li>
+													<div class="inner">
+														<a href="#">
+															<span class="img"><img src="data/smpl5.jpg" alt="" /></span>
+															<span class="title">Name or title of the program</span>
+														</a>
+													</div>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="documentales">
+						documentales
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6">
+		<div class="panel tabs">
+			<div class="panel-title">
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="active"><a href="#peliculas" role="tab" data-toggle="tab"><span>Peliculas</span></a></li>
+					<li><a href="#series" role="tab" data-toggle="tab"><span>Series</span></a></li>
+				</ul>
+			</div>
+			<div class="panel-body">
+				<div class="tab-content">
+					<div class="tab-pane active fade in" id="peliculas">
+						<div class="item video big">
+							<a href="#">
+								<span class="img">
+									<img src="data/smpl3.jpg" alt="" />
+								</span>
+								<span class="title-holder">
+									<span class="title">Irán Hoy</span>
+									<span class="subtitle">Calurosa bienvenida a la 27º Feria del Libro de Teherán</span>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="series">
+						series
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

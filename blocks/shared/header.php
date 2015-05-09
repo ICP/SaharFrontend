f75384d47f5df085c9d813cd6cd69923eb6eccc6
Farid Rn <faridv@gmail.com>
2015-05-09 22:23:58 +0430

    Header changes

--- blocks/shared/header.php
+++ blocks/shared/header.php
@@ -1,18 +1,4 @@
 <header id="header">
-	<div id="navbar" class="wrapper">
-		<div class="container">
-			<div class="row">
-<!--				<div class="col-xs-4 col-sm-3 col-md-2">
-					<h1 id="logo">
-						<a href="#">HispanTV</a>
-					</h1>
-				</div>-->
-				<div class="col-xs-8 col-sm-9 col-md-10">
-					<nav id="menu" class=""><a id="menu-toggle" class="btn btn-default pull-right hidden-md hidden-lg"><i class="icon-menu"></i></a><ul class="list-unstyled list-inline"><li><a href="/">Inicio</a></li><li class="haschild"><a href="/noticias">Noticias</a><div class="child"><div class="row"><div class="col-md-5"><ul class="list-unstyled"><li class="hidden-md hidden-lg"><a href="/noticias">Noticias</a></li><li><a href="/category/Irán/1">Irán</a></li><li><a href="/category/Oriente-Medio/26">Oriente Medio</a></li><li><a href="/category/Asia-y-Oceanía/4">Asia y Oceanía</a></li><li><a href="/category/África/18">África</a></li><li><a href="/category/Europa/35">Europa</a></li><li><a href="/category/América-del-Norte/44">América del Norte</a></li><li><a href="/category/Sudamérica/51">Sudamérica</a></li><li><a href="/category/Centroamérica/103">Centroamérica</a></li></ul></div><div class="col-md-5 col-md-offset-2"><ul class="list-unstyled"><li><a href="/category/Sociedad/82">Sociedad</a></li><li><a href="/category/Economía/80">Economía</a></li><li><a href="/category/Ciencia-Tecnología/86">Ciencia/Tec</a></li><li><a href="/category/Deporte/90">Deporte</a></li><li><a href="/category/Salud/88">Salud</a></li><li><a href="/category/Cultura/84">Cultura</a></li></ul></div></div></div></li><li class="haschild"><a href="/programas">Programas</a><div class="child"><div class="row"><div class="col-md-5"><ul class="list-unstyled"><li class="hidden-md hidden-lg"><a href="/programas">Programas</a></li><li><a href="/showprogram/Iran/97">Irán</a></li><li><a href="/showprogram/Hablemos-Persa/102">Hablemos Persa</a></li><li><a href="/showprogram/Detras-de-la-Razon-/100">Detrás de la Razón</a></li><li><a href="/showprogram/Fort-Apache/106">Fort Apache</a></li><li><a href="/showprogram/Enfoque/98">Enfoque</a></li><li><a href="/showprogram/Mas-que-Deporte/103">Más que Deporte</a></li><li><a href="/showprogram/Iran-Hoy/111">Irán Hoy</a></li><li><a href="/showprogram/Dialogo-Abierto/109">Diálogo Abierto</a></li></ul></div><div class="col-md-5 col-md-offset-2"><ul class="list-unstyled"><li><a href="/showprogram/Distrito-Financiero/112">Distrito Financiero</a></li><li><a href="/showprogram/Cine-a-Contracorriente/107">Cine a Contracorriente</a></li><li><a href="/showprogram/La-Gran-Historia/108">La Gran Historia</a></li><li><a href="/showprogram/¿Que-opinas/105">¿Qué opinas?</a></li><li><a href="/showprogram/Continentes/104">Continentes</a></li><li><a href="/showprogram/Hollywood-Cut/152">Hollywood Cut</a></li><li><a href="/showprogram/Corto-Latino/153">Corto Latino</a></li><li><a href="/showprogram/Somos-UNO/117">Somos UNO</a></li></ul></div></div></div></li><li><a href="/documentales">Documentales</a></li><li class="active"><a href="/reportajes">Reporteros</a></li><li><a href="/peliculas">Cine persa</a></li><li><a href="/series">Series</a></li><li><a href="/fotos">Fotos</a></li><li class="search"><a href="#"><i class="icon-search"></i></a><div class="search-form" style="width: 0;"><form action="/search" method="get"><div class="form-group"><input type="text" name="q" class="form-control bg_grey" placeholder="Search..." /><button type="submit"><i class="icon-search"></i></button></div></form></div></li></ul></nav> 
-				</div>
-			</div>
-		</div>
-	</div>
 	<div id="masthead" class="wrapper">
 		<div class="container">
 			<div class="row">
@@ -31,6 +17,20 @@
 							<li><div class="search"><form action="/search" method="get"><div class="form-group"><input type="text" name="q" class="form-control bg_grey" placeholder="Search..." /><button type="submit"><i class="icon-search"></i></button></div></form></div></li>
 						</ul>
 					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="navbar" class="wrapper">
+		<div class="container">
+			<div class="row">
+<!--				<div class="col-xs-4 col-sm-3 col-md-2">
+					<h1 id="logo">
+						<a href="#">HispanTV</a>
+					</h1>
+				</div>-->
+				<div class="col-xs-8 col-sm-9 col-md-10">
+					<nav id="menu" class=""><a id="menu-toggle" class="btn btn-default pull-right hidden-md hidden-lg"><i class="icon-menu"></i></a><ul class="list-unstyled list-inline"><li><a href="/">Inicio</a></li><li class="haschild"><a href="/noticias">Noticias</a><div class="child"><div class="row"><div class="col-md-5"><ul class="list-unstyled"><li class="hidden-md hidden-lg"><a href="/noticias">Noticias</a></li><li><a href="/category/Irán/1">Irán</a></li><li><a href="/category/Oriente-Medio/26">Oriente Medio</a></li><li><a href="/category/Asia-y-Oceanía/4">Asia y Oceanía</a></li><li><a href="/category/África/18">África</a></li><li><a href="/category/Europa/35">Europa</a></li><li><a href="/category/América-del-Norte/44">América del Norte</a></li><li><a href="/category/Sudamérica/51">Sudamérica</a></li><li><a href="/category/Centroamérica/103">Centroamérica</a></li></ul></div><div class="col-md-5 col-md-offset-2"><ul class="list-unstyled"><li><a href="/category/Sociedad/82">Sociedad</a></li><li><a href="/category/Economía/80">Economía</a></li><li><a href="/category/Ciencia-Tecnología/86">Ciencia/Tec</a></li><li><a href="/category/Deporte/90">Deporte</a></li><li><a href="/category/Salud/88">Salud</a></li><li><a href="/category/Cultura/84">Cultura</a></li></ul></div></div></div></li><li class="haschild"><a href="/programas">Programas</a><div class="child"><div class="row"><div class="col-md-5"><ul class="list-unstyled"><li class="hidden-md hidden-lg"><a href="/programas">Programas</a></li><li><a href="/showprogram/Iran/97">Irán</a></li><li><a href="/showprogram/Hablemos-Persa/102">Hablemos Persa</a></li><li><a href="/showprogram/Detras-de-la-Razon-/100">Detrás de la Razón</a></li><li><a href="/showprogram/Fort-Apache/106">Fort Apache</a></li><li><a href="/showprogram/Enfoque/98">Enfoque</a></li><li><a href="/showprogram/Mas-que-Deporte/103">Más que Deporte</a></li><li><a href="/showprogram/Iran-Hoy/111">Irán Hoy</a></li><li><a href="/showprogram/Dialogo-Abierto/109">Diálogo Abierto</a></li></ul></div><div class="col-md-5 col-md-offset-2"><ul class="list-unstyled"><li><a href="/showprogram/Distrito-Financiero/112">Distrito Financiero</a></li><li><a href="/showprogram/Cine-a-Contracorriente/107">Cine a Contracorriente</a></li><li><a href="/showprogram/La-Gran-Historia/108">La Gran Historia</a></li><li><a href="/showprogram/¿Que-opinas/105">¿Qué opinas?</a></li><li><a href="/showprogram/Continentes/104">Continentes</a></li><li><a href="/showprogram/Hollywood-Cut/152">Hollywood Cut</a></li><li><a href="/showprogram/Corto-Latino/153">Corto Latino</a></li><li><a href="/showprogram/Somos-UNO/117">Somos UNO</a></li></ul></div></div></div></li><li><a href="/documentales">Documentales</a></li><li class="active"><a href="/reportajes">Reporteros</a></li><li><a href="/peliculas">Cine persa</a></li><li><a href="/series">Series</a></li><li><a href="/fotos">Fotos</a></li><li class="search"><a href="#"><i class="icon-search"></i></a><div class="search-form" style="width: 0;"><form action="/search" method="get"><div class="form-group"><input type="text" name="q" class="form-control bg_grey" placeholder="Search..." /><button type="submit"><i class="icon-search"></i></button></div></form></div></li></ul></nav> 
 				</div>
 			</div>
 		</div>

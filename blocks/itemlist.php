c31cda87ce8d8400210bd71f0cd5e509c0e9be7d
Farid Rn <faridv@gmail.com>
2015-05-15 01:26:53 +0430

    Itemlist with title on top

--- blocks/itemlist.php
+++ blocks/itemlist.php
@@ -16,6 +16,22 @@
 	<div class="panel itemlist thumb thumbs-large">
 		<div class="panel-body">
 			<ul class="list-unstyled">
+				<li class="featured title-top">
+					<div class="inner">
+						<div class="title">
+							<h3><a href="#">Podemos: el TTIP es 'golpe de estado encubierto' de grandes empresas</a></h3>
+						</div>
+						<div class="img">
+							<a href="#">
+								<img src="data/smpl3.jpg" alt="" />
+							</a>
+						</div>
+						<div class="desc">
+							<p>El Círculo de Cooperación Internacional de Podemos analizó en Madrid los potenciales efectos políticos, sociales y económicos que tendría la firma del acuerdo Transatlántico de Libre Comercio e Inversión (ATCI, o TTIP por sus siglas en inglés) entre la Unión Europea (UE) y Estados Unidos. </p>
+							<div class="meta">23/11/2014 10:19 GMT</div>
+						</div>
+					</div>
+				</li>
 				<li class="featured">
 					<div class="inner">
 						<div class="img">

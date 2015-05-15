1d471f8526dd5122dc9059957e6000b51fdf9413
Farid Rn <faridv@gmail.com>
2015-05-15 21:06:34 +0430

    Archive page added

---
+++ archive.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>   <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>      <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>      <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"> <!--<![endif]-->
+	<head>
+		<?php include dirname(__FILE__) . '/blocks/shared/head.php' ?><!-- ./head -->
+	</head>
+	<body>
+		<?php include dirname(__FILE__) . '/blocks/shared/header.php' ?><!-- ./header -->
+		<section id="main">
+			<div class="wrapper">
+				<div class="container">
+					<div class="row">
+						<section id="mainbody" class="col-xs-12 col-md-12">
+							<div id="item" class="item satellites">
+								<div class="page-header">
+									<h2>Archive</h2>
+								</div>
+								<div class="item-collpasibles">
+									<div class="panel-group" id="accordion" role="tablist">
+										<div class="panel panel-default">
+											<div class="panel-heading" role="tab">
+												<h4 class="panel-title">
+													<a data-toggle="collapse" data-parent="#accordion" href="#argentina-1">
+														Friday 15, May 2015 <i class="icon-down"></i>
+													</a>
+												</h4>
+											</div>
+											<div id="argentina-1" class="panel-collapse collapse in" role="tabpanel">
+												<div class="panel-body">
+													<div class="panel schedules">
+														<div class="panel-body">
+															<table class="table table-responsive">
+																<thead>
+																	<tr>
+																		<th class="col-xs-4">Broadcast</th>
+																		<th class="col-xs-2">Start</th>
+																		<th class="col-xs-2">End</th>
+																		<th class="col-xs-2"></th>
+																		<th class="col-xs-2"></th>
+																	</tr>
+																</thead>
+																<tbody>
+																	<tr>
+																		<td>
+																			<div class="inner"><strong>Part 1</strong></div>
+																		</td>
+																		<td>00:00 UTC</td>
+																		<td>05:00 UTC</td>
+																		<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																		<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+																	</tr>
+																	<tr>
+																		<td>
+																			<div class="inner"><strong>Part 2</strong></div>
+																		</td>
+																		<td>00:00 UTC</td>
+																		<td>05:00 UTC</td>
+																		<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																		<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+																	</tr>
+																	<tr>
+																		<td>
+																			<div class="inner"><strong>Part 3</strong></div>
+																		</td>
+																		<td>00:00 UTC</td>
+																		<td>05:00 UTC</td>
+																		<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																		<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+																	</tr>
+																	<tr>
+																		<td>
+																			<div class="inner"><strong>Part 4</strong></div>
+																		</td>
+																		<td>00:00 UTC</td>
+																		<td>05:00 UTC</td>
+																		<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																		<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+																	</tr>
+																</tbody>
+															</table>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="panel panel-default">
+											<div class="panel-heading" role="tab">
+												<h4 class="panel-title">
+													<a data-toggle="collapse" data-parent="#accordion" href="#argentina-2">
+														Thursday 14, May 2015 <i class="icon-down"></i>
+													</a>
+												</h4>
+											</div>
+											<div id="argentina-2" class="panel-collapse collapse" role="tabpanel">
+												<div class="panel-body">
+													<table class="table table-responsive">
+														<thead>
+															<tr>
+																<th class="col-xs-4">Broadcast</th>
+																<th class="col-xs-2">Start</th>
+																<th class="col-xs-2">End</th>
+																<th class="col-xs-2"></th>
+																<th class="col-xs-2"></th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>
+																	<div class="inner"><strong>Part 1</strong></div>
+																</td>
+																<td>00:00 UTC</td>
+																<td>05:00 UTC</td>
+																<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+															</tr>
+															<tr>
+																<td>
+																	<div class="inner"><strong>Part 2</strong></div>
+																</td>
+																<td>00:00 UTC</td>
+																<td>05:00 UTC</td>
+																<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+															</tr>
+															<tr>
+																<td>
+																	<div class="inner"><strong>Part 3</strong></div>
+																</td>
+																<td>00:00 UTC</td>
+																<td>05:00 UTC</td>
+																<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+															</tr>
+															<tr>
+																<td>
+																	<div class="inner"><strong>Part 4</strong></div>
+																</td>
+																<td>00:00 UTC</td>
+																<td>05:00 UTC</td>
+																<td><a href="#"><i class="icon-play"></i>&nbsp;Play</a></td>
+																<td><a href="#"><i class="icon-download"></i>&nbsp;Download</a></td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include dirname(__FILE__) . '/blocks/shared/footer.php' ?><!-- ./footer -->
+	</body>
+</html>

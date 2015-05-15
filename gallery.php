e70abbca8545bb2317d6370be425634a33dd0470
Farid Rn <faridv@gmail.com>
2015-05-15 19:17:33 +0430

    Galleries page added

---
+++ gallery.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>   <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>      <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>      <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"> <!--<![endif]-->
+	<head>
+		<?php include dirname(__FILE__) . '/blocks/shared/head.php' ?><!-- ./head -->
+	</head>
+	<body>
+		<?php include dirname(__FILE__) . '/blocks/shared/header.php' ?><!-- ./header -->
+		<section id="main">
+			<div class="wrapper">
+				<div class="container">
+					<div class="row">
+						<section id="mainbody" class="col-xs-12 col-md-12">
+							<div class="page-header">
+								<h2>Galleries</h2>
+							</div>
+							<div id="gallery">
+								<div class="row bg_grey">
+									<div class="col-xs-12">
+										<div id="slideshow" class="row">
+											<div class="col-xs-12">
+												<div class="panel slideshow gallery">
+													<div class="panel-body">
+														<div class="row">
+															<div class="col-xs-12 col-sm-8">
+																<div class="_relative">
+																	<div class="carousel-control">
+																		<a href="#" class="prev"><i class="icon-left"></i></a>
+																		<a href="#" class="next"><i class="icon-right"></i></a>
+																	</div>
+																</div>
+															</div>
+															<div class="col-xs-12 col-sm-4">
+																<div class="_relative">
+																	<div class="pages text-center">
+																		<span class="current">1</span>/<span class="total"></span>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<ul class="list-unstyled">
+															<li class="item">
+																<div class="inner row">
+																	<div class="col-xs-12 col-sm-8">
+																		<div class="img">
+																			<img src="data/smpl11.jpg" alt="" />
+																		</div>
+																	</div>
+																	<div class="col-xs-12 col-sm-4 desc">
+																		<div class="desc">
+																			<h2>12 alimentos inteligentes: cómalos para potenciar su cerebro</h2>
+																			<p>Destellos de relámpagos, flujo de lava ardiente y nubes de cenizas gigantes liberados desde el cráter durante la erupción del volcán Sinabung, en la isla de Sumatra. 14 de octubre 2014.</p>
+																			<div class="date">25 minutes ago</div>
+																			<div class="panel share">
+																				<div class="panel-body">
+																					<ul class="list-unstyled list-inline">
+																						<li><a href="#"><i class="icon-facebook"></i></a></li>
+																						<li><a href="#"><i class="icon-twitter"></i></a></li>
+																						<li><a href="#"><i class="icon-gplus"></i></a></li>
+																					</ul>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</li>
+															<li class="item">
+																<div class="inner row">
+																	<div class="col-xs-12 col-sm-8">
+																		<div class="img">
+																			<img src="data/smpl12.jpg" alt="" />
+																		</div>
+																	</div>
+																	<div class="col-xs-12 col-sm-4 desc">
+																		<div class="desc">
+																			<h2>aev adv iuadvl avljkha vlkja va</h2>
+																			<p>khg kg jkh kjhg kjhg jkhg kjhg jkhg i dvo;iadv ijad v;oiad v;jad v;kladm vlkadn vljhdav kh advkjhb avkjhb advkhba dvlg avhba dvh.</p>
+																			<div class="date">25 minutes ago</div>
+																			<div class="panel share">
+																				<div class="panel-body">
+																					<ul class="list-unstyled list-inline">
+																						<li><a href="#"><i class="icon-facebook"></i></a></li>
+																						<li><a href="#"><i class="icon-twitter"></i></a></li>
+																						<li><a href="#"><i class="icon-gplus"></i></a></li>
+																					</ul>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="panel tiles">
+									<div class="panel-title">
+										<h3>More Galleries</h3>
+									</div>
+									<div class="panel-body">
+										<div class="row">
+											<ul class="list-unstyled">
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+												<li class="tile col-xs-12 col-sm-3">
+													<div class="inner">
+														<div class="img video">
+															<a href="#">
+																<img src="data/smpl8.jpg" alt="" />
+															</a>
+														</div>
+														<div class="desc">
+															<h4><a href="#">Cine a Contra Corriente</a></h4>
+														</div>
+													</div>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="panel-footer">
+										<div class="text-center">
+											<ul class="pagination">
+												<li><a href="#">&laquo;</a></li>
+												<li><a href="#">1</a></li>
+												<li class="active"><a href="#">2</a></li>
+												<li><a href="#">3</a></li>
+												<li><a href="#">&raquo;</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include dirname(__FILE__) . '/blocks/shared/footer.php' ?><!-- ./footer -->
+	</body>
+</html>

c80b42283e0029070863c5256482f4272de21de4
Farid Rn <faridv@gmail.com>
2015-05-19 16:16:39 +0430

    Player module added

---
+++ blocks/modules/player.php
@@ -0,0 +1,15 @@
+<div id="player" class="row">
+    <div class="col-xs-12">
+        <div class="panel top-news">
+            <div class="panel-body">
+                <div class="item">
+                    <div class="inner">
+                        <div class="img video">
+                            <img src="data/smpl10.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
